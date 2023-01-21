028fd705e8c60412959ce75b91d16b778b0fd1e6
David Dunlevy <david.dunlevy@gmail.com>
2023-01-22 09:59:45 +1300

    Working dirty

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,111 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.*;
+
+import com.skilldistillery.foodtruck.entities.Foodtruck;
+
 public class FoodTruckApp {
+	private Foodtruck[] fleet = new Foodtruck[5];
+	private Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello! welcome to the foodtruck app!\nWe'er going to rate some foodtrucks.");
+		FoodTruckApp app = new FoodTruckApp();
+		app.makeTruck();
+		app.menu();
+
+	}
+
+	public void makeTruck() {
+		for (int i = 0; i < fleet.length; i++) {
+			System.out.println("Please enter the foodtruck name. Or 'quit' ");
+			String truckName = sc.next();
+			if (truckName.equalsIgnoreCase("quit")) {
+				break;
+
+			}
+			System.out.println("Please enter the trucks food type.");
+			String foodType = sc.next();
+
+			System.out.println("Please enter a rating from 1-5, with 5 being the best.");
+			int truckRating = sc.nextInt();
+			sc.nextLine();
+
+			Foodtruck truck = new Foodtruck(truckName, foodType, truckRating);
+			fleet[i] = truck;
+
+		}
+
+	}
+
+	private void avgTruckRating() {
+		// calculate the average
+		double sum = 0;
+		for (int i = 0; i < fleet.length; i++) {
+			if (fleet[i] != null) {
+				sum = sum + fleet[i].getTruckRating();
+			}
+		}
+
+		double avg = sum / fleet.length;
+
+		System.out.println("The average truck rating is " + avg);
+
+	}
+
+	private void highestRated() {
+		// calculate the highest rated
+		Foodtruck highest = fleet[0];
+		for (int i = 0; i < fleet.length; i++) {
+			if (fleet[i] != null) {
+				if (highest.getTruckRating() < fleet[i].getTruckRating()) {
+					highest = fleet[i];
+				}
+			}
+
+		}
+		System.out.println("The highest rated truck is " + highest);
+
+	}
+
+	public void menu() {
+		// menu
+		boolean go = true;
+
+		do {
+
+			System.out.println("1: List all existing food trucks. ");
+			System.out.println("2: See the average rating of food trucks. ");
+			System.out.println("3: Display the highest-rated food truck. ");
+			System.out.println("4: Quit. ");
+			int userIn = sc.nextInt();
+			switch (userIn) {
+
+			case (1):
+				for (int i = 0; i < fleet.length; i++) {
+					if (fleet[i] == null) {
+						continue;
+					} else
+						System.out.println(fleet[i].toString());
+				}
+				break;
+
+			case (2):
+				avgTruckRating();
+				break;
+
+			case (3):
+				highestRated();
+				break;
+
+			case (4):
+				System.out.println("Goodbye");
+				sc.close();
+				go = false;
+			}
+		} while (go == true);
+		return;
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -1,5 +1,64 @@
 package com.skilldistillery.foodtruck.entities;
 
+import java.util.*;
+
 public class Foodtruck {
+	
+	private static int nextTruckId = 0;
+	private int truckId =1;
+	private String truckName;
+	private int truckRating;
+	private String foodType;
+
+	public void setTruckId(int truckId) {
+		this.truckId = nextTruckId;
+		}
+	
+	public int getTruckId() {
+		return truckId;
+		
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public int getTruckRating() {
+		return truckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public Foodtruck( String truckName, String foodType, int truckRating) {
+		super();
+		this.truckId = nextTruckId;
+		nextTruckId++;
+		this.truckName = truckName;
+		this.truckRating = truckRating;
+		this.foodType = foodType;
+	}
+
+	public Foodtruck() {
+	}
+
+	@Override
+	public String toString() {
+		return "Foodtruck [truckid=" + truckId + ", truckName=" + truckName + ", truckRating=" + truckRating
+				+ ", foodType=" + foodType + "]";
+	}
 
 }
